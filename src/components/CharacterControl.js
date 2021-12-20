0b23f5d2dba73ae34ba833871e983a7d82cd4dbd
Ben Wilson <benjaminw1030@gmail.com>
2021-12-19 18:26:28 -0800

    work on properly displaying charactercontrol

--- src/components/CharacterControl.js
+++ src/components/CharacterControl.js
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useFirestore } from "react-redux-firebase";
 import { useSelector, useDispatch } from "react-redux";
+import { withFirestore, isLoaded } from "react-redux-firebase";
 import * as a from "../actions";
 import SidebarMenu from "./SidebarMenu";
-import SidebarChar from "./SidebarChar";
 import NewChar from "./NewChar";
 import CharList from "./CharList";
 import Options from "./Options";
 import CharInfo from "./CharInfo";
+import { getAuth } from "firebase/auth";
 
-export default function CharacterControl() {
+function CharacterControl() {
+  const firestore = useFirestore();
+  const dispatch = useDispatch();
+  const character = useSelector((state) => state.character);
+  const [shownComponent, setShownComponent] = useState("none");
+  const auth = getAuth();
+
+  // console.log(auth.currentUser.uid);
+  let mainField;
+
+  if (!isLoaded(auth)) {
+    mainField = (
+      <>
+        <p>Loading</p>
+      </>
+    );
+  } else if (isLoaded(auth) && auth.currentUser === null) {
+    mainField = (
+      <>
+        <p>Log in to add and view characters.</p>
+      </>
+    );
+  } else if (shownComponent === "newchar") {
+    mainField = (
+      <>
+        <NewChar />
+      </>
+    );
+  } else if (shownComponent === "charlist") {
+    mainField = (
+      <>
+        <CharList />
+      </>
+    );
+  } else if (character != null && shownComponent === "charinfo") {
+    mainField = (
+      <>
+        <CharInfo />
+      </>
+    );
+  } else {
+    mainField = (
+      <>
+        <p>Load or create a character to see details.</p>
+      </>
+    )
+  }
+
   return (
-    <div>
+    <>
       <SidebarMenu />
-      <SidebarChar />
-      <NewChar />
-      <CharInfo />
-      <CharList />
-      <Options />
-    </div>
+      <div>
+        <div>{mainField}</div>
+      </div>
+    </>
   );
 }
+
+export default CharacterControl;
+//potentially add in options later
--- src/components/SidebarChar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SidebarMenu() {
-  return (
-    <div>
-      <p>character sidebar buttons here</p>
-    </div>
-  )
-}
